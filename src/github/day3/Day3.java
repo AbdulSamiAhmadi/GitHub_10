b895995526ad94246f8e24abbf57657ef2257b57
samiahmadi <samiahmadi@10.0.0.189>
2023-07-30 11:34:38 -0400

    Day-3

---
+++ src/github/day3/Day3.java
@@ -0,0 +1,39 @@
+package github.day3;
+
+public class Day3 {
+
+	
+	String name = "Ahmad";
+	String Country = "Afghanistan";
+	
+	public static void main(String[] args) {
+
+	String title = "Director";
+	String author = "Ahmadi";
+	int pageNum = 200;
+	String isbn = "ALB0098RB";
+	int publishyear= 2023;
+	
+	System.out.println(title);
+	System.out.println(author);
+	System.out.println(pageNum);
+	System.out.println(isbn);
+	System.out.println(publishyear);	
+	
+	}
+	
+	public  void Subtract () {
+
+		int s1 = 10;
+		int s2 = 50;
+		int s3 = 100;
+		
+		int result = s1 + s2 + s3;
+		
+		System.out.println(result);
+	}
+	
+	
+}
+
+
